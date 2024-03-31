2e56b4a14a85a3b89a4c9c6d7d9b8776ca88465e
NguyenMinhDuc203 <minhduc5068@gmail.com>
2024-03-31 13:00:56 +0700

    finish cau 1a: Course service

---
+++ CAU_1/CourseService/src/main/java/com/exemple/CourseService/entity/Course.java
@@ -0,0 +1,35 @@
+package com.exemple.CourseService.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "EducationDB")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Course {
+    private Long SubId;
+    private String Name;
+    private double Price;
+    private String Description;
+
+    public Course(String name, double price, String description) {
+        this.Name = name;
+        this.Price = price;
+        this.Description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "SubId=" + SubId +
+                ", Name='" + Name + '\'' +
+                ", Price=" + Price +
+                ", Description='" + Description + '\'' +
+                '}';
+    }
+}
